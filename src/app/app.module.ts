99fc39177356b8d1af9beac2a31dc10f170c82c3
Steve Hall <shall4@slb.com>
2017-11-02 17:52:24 +0200

    Add some comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,21 +17,27 @@ import { HeroSearchComponent } from './hero-search.component';
 import { AppRoutingModule }    from './app-routing.module';
 
 @NgModule({
-    imports: [
+    // exports: the subset of declarations that should be visible and usable in the component templates of other modules.
+    //exports: [ ]
+    // imports: other modules whose exported classes are needed by component templates declared in this module.
+    imports: [ 
         BrowserModule,
         FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
         AppRoutingModule,
         HttpModule,
         InMemoryWebApiModule.forRoot(InMemoryDataService),
     ],
-    declarations: [
+    // declarations: the view classes that belong to this module. Angular has three kinds of view classes: components, directives, and pipes.
+    declarations: [ 
         AppComponent,
         HeroDetailComponent,
         HeroesComponent,
         DashboardComponent,
         HeroSearchComponent
     ],
-    providers: [ HeroService ],  
+    // providers: creators of services that this module contributes to the global collection of services; they become accessible in all parts of the app.
+    providers: [ HeroService ],
+    // bootstrap: the main application view, called the root component, that hosts all other app views. Only the root module should set this bootstrap property.  
     bootstrap: [ AppComponent ]
     
 })
